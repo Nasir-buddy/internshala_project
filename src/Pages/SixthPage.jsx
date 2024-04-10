985d800bf6d644c2963ed2aaa849003657c7ec9a
nasir-buddy <syedali172000@gmail.com>
2024-04-10 16:32:35 +0530

    internshala: 'implemented 6th page'

---
+++ src/Pages/SixthPage.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const SixthPage = () => {
+  return (
+    <div className='w-screen h-screen flex flex-col items-center justify-center p-2'>
+        <div className='h-[13vh] w-[13vh] border-[13px] border-yellow-500 rounded-full'>
+        </div>
+        <div className='font-semibold text-2xl mt-10'> Finding learning path recommendations for you based on <br /> your responses</div>
+    </div>
+  )
+}
+
+export default SixthPage
